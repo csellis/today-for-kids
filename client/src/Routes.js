edbd1017943f0cb41c549345ecc40321b38f8a91
Chris Ellis <ellis.cs@gmail.com>
2019-02-11 11:47:27 -0500

    Fix BrowserRouter

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 import Home from "./components/Home";
 
 const Routes = () => (
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 import Provider from "./Provider";
@@ -7,7 +9,9 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
