e8393f76c1f2ab96737cb33a4c603e5c3f1c64b6
Chris Ellis <ellis.cs@gmail.com>
2019-02-13 19:55:33 -0500

    setTemp in local storage

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,19 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Paper from "@material-ui/core/Paper";
 import Slider from "@material-ui/lab/Slider";
 import store from "store";
 import { withStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
 
 function Settings(props) {
-  const storeTemp = store.get("temp");
+  const storeTemp = store.get("temp") || 65;
   const [temp, setTemp] = useState(storeTemp);
 
   const { classes } = props;
+  let handleChange = (event, value) => {
+    event.preventDefault(true);
+    setTemp(value);
+    store.set("temp", value);
+  };
   return (
-    <Paper className={classes.container}>
-      <Slider value={temp} min={55} max={75} step={1} onChange={setTemp} />
-    </Paper>
+    <div className={classes.container}>
+      <Typography variant='h5' gutterBottom>
+        Select Temperature
+      </Typography>
+      <Typography variant='subtitle1' gutterBottom>
+        Select the temperature at which your child will start wearing clothes for warm weather.
+      </Typography>
+      <div className={classes.sliderContainer}>
+        <Slider
+          value={temp}
+          min={55}
+          max={75}
+          step={1}
+          onChange={handleChange}
+          className={classes.slider}
+        />
+        <h2>{temp}&deg;F</h2>
+      </div>
+    </div>
   );
 }
 
@@ -21,7 +43,17 @@ const styles = theme => ({
   container: {
     display: "flex",
     flexWrap: "wrap",
-    height: "100vh"
+    height: "100vh",
+    flexDirection: "column",
+    margin: `${theme.spacing.unit * 2}px auto`,
+    alignItems: "center"
+  },
+  sliderContainer: {
+    marginTop: theme.spacing.unit * 2
+  },
+  slider: {
+    width: "300px",
+    touchAction: "none"
   },
   textField: {
     marginLeft: theme.spacing.unit,
