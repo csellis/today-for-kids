1601cffc8cff39e0da883f37c24475fee6f9138f
Chris Ellis <ellis.cs@gmail.com>
2019-02-25 09:27:43 -0500

    Remove excess code in weatherRoutes

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -1,23 +1,5 @@
-const _ = require("lodash");
-const Path = require("path-parser");
 const axios = require("axios");
-const { URL } = require("url");
-const mongoose = require("mongoose");
 const keys = require("../config/keys");
-
-const Weather = mongoose.model("weathers");
-
-const fetchWeatherFromOpenWeatherApi = async location => {
-  try {
-    return await axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?zip=${location},us&APPID=${
-        keys.openWeatherMap
-      }`
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 const fetchWeatherFromDarkSky = async (latitude, longitude) => {
   try {
@@ -33,12 +15,7 @@ const fetchWeatherFromDarkSky = async (latitude, longitude) => {
 
 module.exports = app => {
   app.get("/api/getWeather/:latitude/:longitude", async (req, res) => {
-    // console.log(
-    //   `Location: ${req.params.latitude}, ${req.params.longitude}, userId: ${req.params.userId}`
-    // );
     const { latitude, longitude } = req.params;
-
-    console.log("fetching new data");
     let weatherFromApi = await fetchWeatherFromDarkSky(latitude, longitude);
 
     try {
